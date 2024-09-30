6f7ff84829c30a3cd270b63687c6e2a6048b003f
LuffySW <luthfisatrio53@gmail.com>
2024-09-30 09:58:32 +0700

    io ke fix cuyyyyy

--- src/Utility/IO.java
+++ src/Utility/IO.java
@@ -5,11 +5,14 @@ import Matrix.*;
 
 public class IO {
 
-  public static File[] getListDir(){
-    File curDir = new File("../test");
-    File[] listFiles = curDir.listFiles();
+  public static File[] getListDir() {
+    // Use the current working directory and append the 'test' folder
+    String currentDir = System.getProperty("user.dir"); // Gets the current working directory
+    File curDir = new File(currentDir, "test"); // Create a File object for the 'test' directory
+    File[] listFiles = curDir.listFiles(); // List files in the 'test' directory
     return listFiles;
   }
+
 
   public static void printListDir(){
     File[] listFiles = getListDir();
@@ -20,65 +23,71 @@ public class IO {
 
   public static int readRow(String s) {
     int count = 0;
-    try{
-      FileReader reader = new FileReader(String.format("../test/%s", s));
+    try {
+      // Use user.dir to construct the file path
+      FileReader reader = new FileReader(String.format("%s/test/%s", System.getProperty("user.dir"), s));
       BufferedReader bufferReader = new BufferedReader(reader);
-  
-      while(bufferReader.readLine() != null){
+
+      while (bufferReader.readLine() != null) {
         count++;
       }
       reader.close();
-    } catch(IOException e){
+    } catch (IOException e) {
       e.printStackTrace();
     }
     return count;
   }
 
-  public static int readCol(String s){
+  public static int readCol(String s) {
     int count = 0;
-    try{
-      FileReader reader = new FileReader(String.format("../test/%s", s));
+    try {
+      // Use user.dir to construct the file path
+      FileReader reader = new FileReader(String.format("%s/test/%s", System.getProperty("user.dir"), s));
       BufferedReader bufferReader = new BufferedReader(reader);
-      
+
       String line = bufferReader.readLine();
-      String[] lines = line.split(" ");
-      count = lines.length;
+      if (line != null) {
+        String[] lines = line.split(" ");
+        count = lines.length;
+      }
       reader.close();
-    } catch(IOException e){
+    } catch (IOException e) {
       e.printStackTrace();
     }
     return count;
   }
 
-  public static Matrix readMatrix(String s){
+  public static Matrix readMatrix(String s) {
     Matrix a = new Matrix(readRow(s), readCol(s));
 
     try {
-      FileReader reader = new FileReader(String.format("../test/%s", s));
+      // Use user.dir to construct the file path
+      FileReader reader = new FileReader(String.format("%s/test/%s", System.getProperty("user.dir"), s));
       BufferedReader bufferReader = new BufferedReader(reader);
 
       String line;
       int count = 0;
-      while((line = bufferReader.readLine()) != null){
+      while ((line = bufferReader.readLine()) != null) {
         String[] lines = line.split(" ");
-        for(int i = 0; i < lines.length; i++){
+        for (int i = 0; i < lines.length; i++) {
           double temp = Operation.eval(lines[i]);
           a.setElmt(count, i, temp);
         }
         count++;
       }
       reader.close();
-    } catch(IOException e){
+    } catch (IOException e) {
       e.printStackTrace();
     }
     return a;
   }
 
-  public static void writeFileMatrix(String namaFile, Matrix m){
-    try{
-      FileWriter writer = new FileWriter(String.format("../test/%s.txt", namaFile));
-      for(int i = 0; i < m.getRow(); i++){
-        for(int j = 0; j < m.getCol(); j++){
+  public static void writeFileMatrix(String namaFile, Matrix m) {
+    try {
+      // Use user.dir to construct the file path
+      FileWriter writer = new FileWriter(String.format("%s/test/%s.txt", System.getProperty("user.dir"), namaFile));
+      for (int i = 0; i < m.getRow(); i++) {
+        for (int j = 0; j < m.getCol(); j++) {
           String temp = Double.toString(m.getElmt(i, j));
           writer.write(temp);
           writer.write(" ");
@@ -86,14 +95,15 @@ public class IO {
         writer.write("\n");
       }
       writer.close();
-    }catch(IOException e){
+    } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
   public static void writeFileString(String namaFile, String s) {
     try {
-      FileWriter writer = new FileWriter(String.format("../test/%s.txt", namaFile));
+      // Use user.dir to construct the file path
+      FileWriter writer = new FileWriter(String.format("%s/test/%s.txt", System.getProperty("user.dir"), namaFile));
       writer.write(s);
       writer.close();
     } catch (IOException e) {
